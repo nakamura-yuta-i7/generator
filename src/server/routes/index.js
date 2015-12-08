412dd0d410ff590fa09926dbb006ed17c5442bf9
Yuta Nakamura <yuta.nakamura.i7@gmail.com>
2015-12-08 14:28:05 +0000

    co & mysql

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+	
 	var co = require('co');
 	var thunkify = require ("thunkify")
 	
--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var co = require("co")
+var thunkify = require("thunkify")
+var db = require("../libs/mysql")
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	var query = thunkify(db.query.bind(db))
+	
+	co(function *() {
+		var result = yield query("select * from users")
+		res.json(result[0])
+	})
 });
 
 module.exports = router;
